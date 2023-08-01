b8b5da4226f6d7393720e5381bd248491b0c08cb
Johnatan(oficina) <jarango@ecobioplast.com>
2023-08-01 17:06:45 -0500

    Pqueño avance en la oficina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { Hero } from './components/Hero'
+import { About } from "./components/About";
+
+import './styles/app.css'
 
 const App = () => {
     return (
         <>
             <Header />
             <Hero /> 
+            <About />
         </>
     )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import '../styles/about.css'
 
 const About = () => {
-    <div className="about__container">
-        <div className="about__section">
-            <div className="about__title">
-                Sobre mí
+    return (
+        <div className="about">
+            <div className="about__section">
+                <div className="about__title">
+                    Sobre mí
+                </div>
+                <div className="about__content">
+                    Soy un ingeniero químico de profesión, apasionado por la programación; hace un poco más de dos años empecé a estudiar desarrollo web con diversos contenidos y cursos virtuales. 
+                </div>
             </div>
-            <div className="about__content">
-                Soy un ingeniero químico de profesión, apasionado por la programación; hace un poco más de dos años empecé a estudiar desarrollo web con diversos contenidos y cursos virtuales. 
+            <div className="about__section">
+                <div className="about__title">
+                    Educación
+                </div>
+                <div className="about__education">
+                        <div className="education-name"> Universidad Nacional de Colombia</div>
+                        
+                </div>
             </div>
         </div>
-        <div className="about__section">
-            <div className="about__title">
-                Educación
-            </div>
-            <div className="about__education">
-                    <div className="education-name"></div>
-            </div>
-        </div>
-    </div>
+    )
 }
 
 export { About }
