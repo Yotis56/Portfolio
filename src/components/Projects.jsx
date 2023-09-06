757f7bfc10994887866d2010d148aa22d0b3be74
Johnatan(oficina) <jarango@ecobioplast.com>
2023-09-06 16:02:21 -0500

    Se empieza a añadir info de los proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
 import { ProjectCard } from "./ProjectCard";
-import testImage from '../assets/testImage.png'
+import { projecstList } from "../projectsList";
 import '../styles/projects.scss'
 
 
 
 const Projects = () => {
-    const [project, useProject] = useState({
-        image: testImage, 
-        title: 'Proyecto de prueba', 
-        description: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
-        techStack: 'React, Redux, Sass', 
-        linkDemo: '#', 
-        linkGithub: '#' 
-    })
+    const [projects, useProject] = useState(projecstList)
+    // {
+    //     image: testImage, 
+    //     title: 'Proyecto de prueba', 
+    //     description: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
+    //     techStack: 'React, Redux, Sass', 
+    //     linkDemo: '#', 
+    //     linkGithub: '#' 
+    // }
+
     return (
         <div className="projects" id="proyectos">
             <div className="title title-centered">Proyectos</div>
             <div className="paragraph">Estos son algunos proyectos en los que he trabajado, te invito a que les des un vistazo</div>
             <div className="projects__container">
-               <ProjectCard project={project} /> 
-               <ProjectCard project={project} /> 
-               <ProjectCard project={project} /> 
-               <ProjectCard project={project} /> 
-               <ProjectCard project={project} /> 
-               <ProjectCard project={project} /> 
+                {
+                    projects.map(project => <ProjectCard project={project} id={project.id}/>)
+                }
             </div>
         </div>
     )
---
+++ src/projectsList.js
@@ -0,0 +1,58 @@
+import testImage from './assets/testImage.png'
+
+export const projecstList = [
+    {
+        id: 0,
+        image: testImage, 
+        title: 'Vuela Fácil - FrontEnd', 
+        description: 'Proyecto para el programa de Misión Tic 2021. Se emula un sitio web de una aerolínea con un sistema de agendamiento de vuelos para los usuarios, y un sistema de administración de la base de datos para un administrador ', 
+        techStack: 'Angular, CSS, Typescript', 
+        linkDemo: 'https://yotis56.github.io/VuelaFacilC4-FrontEnd/', 
+        linkGithub: 'https://github.com/Yotis56/VuelaFacilC4-FrontEnd'
+    },
+    {
+        id: 1,
+        image: testImage, 
+        title: 'Vuela Fácil - BackEnd', 
+        description: 'Proyecto para el programa Misión TIC 2021. Se genera una aplicación backend con Express.js que gestiona las peticiones CRUD que hace el Frontend y conecta con una base de datos en Mongo DB', 
+        techStack: 'Mongo, Express.js, Typescript', 
+        linkDemo: '#', 
+        linkGithub: 'https://github.com/Yotis56/VuelaFacilC4-BackEnd'
+    },
+    {
+        id: 2,
+        image: testImage, 
+        title: 'Platzi Video', 
+        description: 'Proyecto del curso de React de Platzi. Se construyó una página web con react y redux en donde se simula la llamada a una API, la renderización de información (componentes con videos) y el manejo de un estado global con Redux', 
+        techStack: 'React, Redux, Sass, webpack, babel', 
+        linkDemo: 'https://yotis56.github.io/Platzi-video-react-/', 
+        linkGithub: 'https://github.com/Yotis56/Platzi-video-react-/tree/router-redux'
+    },
+    {
+        id: 3,
+        image: testImage, 
+        title: 'Pokedux', 
+        description: 'Proyecto desarrollado en el curso de Redux de Platzi. Se hace un llamado a una api pública de pókemon, se maneja un estado global (resultados de búsqueda, favoritos e info completa) y el conjunto se renderiza con React', 
+        techStack: 'React, Redux, CSS, webpack', 
+        linkDemo: '#', 
+        linkGithub: '#'
+    },
+    {
+        id: 4,
+        image: testImage, 
+        title: 'TODO App', 
+        description: 'Ejemplo clásico de una aplicación para guardar TODOs. Se le da persistencia a los datos usando el local storage, uso una navegación básica con react router', 
+        techStack: 'React, Redux, Sass', 
+        linkDemo: '#', 
+        linkGithub: '#'
+    },
+    {
+        id: 5,
+        image: testImage, 
+        title: 'Proyecto de prueba', 
+        description: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
+        techStack: 'React, Redux, Sass', 
+        linkDemo: '#', 
+        linkGithub: '#'
+    }
+]
